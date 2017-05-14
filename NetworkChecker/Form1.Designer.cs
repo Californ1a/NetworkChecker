1cfecc2305651f5bf70fa31d8ee9f7b9d569952f
Californ1a <rk_california@yahoo.com>
2017-05-14 08:25:52 -0400

    Add project files.

---
+++ NetworkChecker/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace NetworkChecker
+{
+    partial class NetworkChecker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NetworkChecker));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Log = new System.Windows.Forms.TabPage();
+            this.swapTimes = new System.Windows.Forms.TabPage();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.totalSwaps = new System.Windows.Forms.ToolStripStatusLabel();
+            this.swapsPerHour = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lastSwapTS = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tabControl1.SuspendLayout();
+            this.Log.SuspendLayout();
+            this.swapTimes.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.richTextBox1.HideSelection = false;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.Size = new System.Drawing.Size(554, 175);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.Log);
+            this.tabControl1.Controls.Add(this.swapTimes);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(568, 207);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // Log
+            // 
+            this.Log.Controls.Add(this.richTextBox1);
+            this.Log.Location = new System.Drawing.Point(4, 22);
+            this.Log.Name = "Log";
+            this.Log.Padding = new System.Windows.Forms.Padding(3);
+            this.Log.Size = new System.Drawing.Size(560, 181);
+            this.Log.TabIndex = 0;
+            this.Log.Text = "Log";
+            this.Log.UseVisualStyleBackColor = true;
+            // 
+            // swapTimes
+            // 
+            this.swapTimes.Controls.Add(this.richTextBox2);
+            this.swapTimes.Location = new System.Drawing.Point(4, 22);
+            this.swapTimes.Name = "swapTimes";
+            this.swapTimes.Size = new System.Drawing.Size(560, 181);
+            this.swapTimes.TabIndex = 1;
+            this.swapTimes.Text = "Swap Times";
+            this.swapTimes.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.richTextBox2.HideSelection = false;
+            this.richTextBox2.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox2.Size = new System.Drawing.Size(560, 181);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.totalSwaps,
+            this.swapsPerHour,
+            this.lastSwapTS});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 208);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(568, 24);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // totalSwaps
+            // 
+            this.totalSwaps.BackColor = System.Drawing.Color.Transparent;
+            this.totalSwaps.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.totalSwaps.Name = "totalSwaps";
+            this.totalSwaps.Size = new System.Drawing.Size(81, 19);
+            this.totalSwaps.Text = "Total Swaps: 0";
+            // 
+            // swapsPerHour
+            // 
+            this.swapsPerHour.BackColor = System.Drawing.Color.Transparent;
+            this.swapsPerHour.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Left;
+            this.swapsPerHour.BorderStyle = System.Windows.Forms.Border3DStyle.Etched;
+            this.swapsPerHour.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.swapsPerHour.Name = "swapsPerHour";
+            this.swapsPerHour.Size = new System.Drawing.Size(145, 19);
+            this.swapsPerHour.Text = "Avg Swaps Per Hour: 0.00";
+            // 
+            // lastSwapTS
+            // 
+            this.lastSwapTS.ActiveLinkColor = System.Drawing.Color.Red;
+            this.lastSwapTS.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Left;
+            this.lastSwapTS.BorderStyle = System.Windows.Forms.Border3DStyle.Etched;
+            this.lastSwapTS.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.lastSwapTS.Name = "lastSwapTS";
+            this.lastSwapTS.Size = new System.Drawing.Size(98, 19);
+            this.lastSwapTS.Text = "Last Swap: None";
+            // 
+            // NetworkChecker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.ClientSize = new System.Drawing.Size(568, 232);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tabControl1);
+            this.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "NetworkChecker";
+            this.ShowIcon = false;
+            this.Text = "NetworkChecker";
+            this.tabControl1.ResumeLayout(false);
+            this.Log.ResumeLayout(false);
+            this.swapTimes.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage Log;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel totalSwaps;
+        private System.Windows.Forms.ToolStripStatusLabel swapsPerHour;
+        private System.Windows.Forms.TabPage swapTimes;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.ToolStripStatusLabel lastSwapTS;
+    }
+}
+
---
+++ NetworkChecker/Form1.cs
@@ -0,0 +1,411 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net.NetworkInformation;
+using System.IO;
+using System.Diagnostics;
+using System.Security.Principal;
+
+namespace NetworkChecker
+{
+    public partial class NetworkChecker : Form
+    {
+        NotifyIcon theIcon;
+        Icon activeIcon;
+        Icon idleIcon;
+        Thread threadWorker;
+
+        public static string _path = Directory.GetCurrentDirectory() + "\\Logs";
+        public static string _txtPath = _path += "\\swaps.log";
+        public static string _interface1 = "6";
+        public static string _interface2 = "7";
+        public static int _seconds = 60;
+        public static int _totalSwaps = 0;
+        public static double _swapsPerHour = 0.00;
+        public static DateTime _lastSwapTime;
+        public static DateTime _runtime = DateTime.Now;
+
+        public NetworkChecker()
+        {
+            InitializeComponent();
+            
+
+            activeIcon = new Icon("Active.ico");
+            idleIcon = new Icon("Idle.ico");
+            theIcon = new NotifyIcon();
+
+            
+
+            theIcon.BalloonTipClicked += TheIcon_BalloonTipClicked;
+
+            richTextBox1.KeyDown += RichTextBox1_KeyDown;
+
+            theIcon.MouseDoubleClick += MainMouseDoubleClick;
+            theIcon.Icon = idleIcon;
+            theIcon.Visible = true;
+
+            MenuItem quitMenuItem = new MenuItem("Quit");
+            MenuItem showMenuItem = new MenuItem("Show");
+            ContextMenu contextMenu = new ContextMenu();
+            contextMenu.MenuItems.Add(showMenuItem);
+            contextMenu.MenuItems.Add(quitMenuItem);
+
+            theIcon.ContextMenu = contextMenu;
+
+            quitMenuItem.Click += QuitMenuItem_Click;
+            showMenuItem.Click += ShowMenuItem_Click;
+
+            this.WindowState = FormWindowState.Minimized;
+            //this.ShowInTaskbar = false;
+
+            threadWorker = new Thread(new ThreadStart(threadStuff));
+            threadWorker.Start();
+
+            if (!IsAdministrator())
+            {
+                DialogResult result = MessageBox.Show("You must run this as Admin.", "Missing permissions", MessageBoxButtons.OK);
+                if (result != DialogResult.None)
+                {
+                    this.Close();
+                }
+            }
+        }
+
+        public static bool IsAdministrator()
+        {
+            return (new WindowsPrincipal(WindowsIdentity.GetCurrent()))
+                    .IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        private void TheIcon_BalloonTipClicked(object sender, EventArgs e)
+        {
+            ShowWindow();
+        }
+
+        private void RichTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            e.SuppressKeyPress = true;
+        }
+
+        private void ShowMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowWindow();
+        }
+
+        private void QuitMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            bool cursorNotInBar = Screen.GetWorkingArea(this).Contains(Cursor.Position);
+
+            if (this.WindowState == FormWindowState.Minimized && cursorNotInBar)
+            {
+                this.WindowState = FormWindowState.Minimized;
+                this.ShowInTaskbar = false;
+                theIcon.Visible = true;
+                this.Hide();
+            }
+        }
+
+        private void MainMouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            ShowWindow();
+        }
+
+        private void ShowWindow()
+        {
+            try
+            {
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+                this.ShowInTaskbar = true;
+                theIcon.Visible = true;
+                richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                richTextBox1.ScrollToCaret();
+                richTextBox2.SelectionStart = richTextBox2.Text.Length;
+                richTextBox2.ScrollToCaret();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        //public delegate void TextBoxDelegate(string message);
+
+        //public void WriteLine(string msg)
+        //{
+        //    //DateTime now = DateTime.Now;
+        //    //string msg = message;
+        //    if (richTextBox1.InvokeRequired)
+        //    {
+        //        richTextBox1.Invoke(new TextBoxDelegate(WriteLine), new object[] { msg });
+        //    }
+        //    else
+        //    {
+        //        if (richTextBox1.Lines.Length >= 300)
+        //        {
+        //            int numOfLines = 30;
+        //            string[] lines = richTextBox1.Lines;
+        //            var newLines = lines.Skip(numOfLines);
+        //            richTextBox1.Lines = newLines.ToArray();
+        //        }
+        //        DateTime now = DateTime.Now;
+        //        richTextBox1.AppendText(now + " | " + msg + Environment.NewLine);
+        //        richTextBox1.SelectionStart = richTextBox1.Text.Length;
+        //        richTextBox1.ScrollToCaret();
+
+        //        //in case if you want to ADD text to previuos one do:
+        //        //textBox1.Text += msg + " ";
+
+        //        //if (InvokeRequired)
+        //        //{
+        //        //    this.Invoke(new Action<string>(WriteLine), new object[] { msg });
+        //        //    return;
+        //        //}
+        //        //DateTime now = DateTime.Now;
+        //        //richTextBox1.Text += now + " | " + msg;
+        //    }
+        //}
+
+        public delegate void TextBoxDelegate(string message, RichTextBox box);
+
+        public void WriteLine(string msg, RichTextBox box)
+        {
+            //DateTime now = DateTime.Now;
+            //string msg = message;
+            if (box.InvokeRequired)
+            {
+                box.Invoke(new TextBoxDelegate(WriteLine), new object[] { msg, box });
+            }
+            else
+            {
+                if (box.Lines.Length >= 300)
+                {
+                    int numOfLines = 30;
+                    string[] lines = box.Lines;
+                    var newLines = lines.Skip(numOfLines);
+                    box.Lines = newLines.ToArray();
+                }
+                DateTime now = DateTime.Now;
+                box.AppendText(now + " | " + msg + Environment.NewLine);
+                box.SelectionStart = richTextBox1.Text.Length;
+                box.ScrollToCaret();
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+
+            if (e.CloseReason == CloseReason.WindowsShutDown) return;
+
+            //threadWorker.Interrupt();
+            //if (!threadWorker.Join(2000))
+            //{
+            //    threadWorker.Abort();
+            //}
+            threadWorker.Abort();
+            theIcon.Dispose();
+        }
+
+        public void threadStuff()
+        {
+            try
+            {
+                //DateTime dt;
+                while (true)
+                {
+                    MinuteToolStrip();
+                    if (PingHost("google.com"))
+                    {
+                        Thread.Sleep(_seconds * 1000);
+                        continue;
+                    }
+                    theIcon.Icon = activeIcon;
+                    WriteLine("Failed to ping Google. Attempting to ping Twitter...", richTextBox1);
+                    if (PingHost("twitter.com"))
+                    {
+                        theIcon.Icon = idleIcon;
+                        Thread.Sleep(_seconds * 1000);
+                        continue;
+                    }
+                    WriteLine("Failed to ping Twitter. Attempting to swap network interfaces...", richTextBox1);
+                    NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+                    foreach (NetworkInterface adapter in interfaces)
+                    {
+                        if (adapter.Name.EndsWith(_interface1))
+                        {
+                            SwapInterfaces(_interface2, _interface1);
+                        }
+                        else if (adapter.Name.EndsWith(_interface2))
+                        {
+                            SwapInterfaces(_interface1, _interface2);
+                        }
+                    }
+                    UpdateToolstrip();
+                    theIcon.ShowBalloonTip(5000, "Network Adapter Changed", "The active network adapter has switched.", ToolTipIcon.Info);
+                    theIcon.Icon = idleIcon;
+                    Thread.Sleep(_seconds * 1000);
+                }
+            }
+            catch (ThreadAbortException tbe)
+            {
+                
+            }
+        }
+
+        public bool PingHost(string nameOrAddress)
+        {
+            bool pingable = false;
+            Ping pinger = new Ping();
+            try
+            {
+                PingReply reply = pinger.Send(nameOrAddress);
+                pingable = reply.Status == IPStatus.Success;
+                if (pingable)
+                {
+                    WriteLine("Heartbeat " + nameOrAddress + " " + reply.RoundtripTime + "ms.", richTextBox1);
+                }
+            }
+            catch (PingException)
+            {
+                // Discard PingExceptions and return false;
+            }
+            return pingable;
+        }
+
+        public void SwapInterfaces(string intToEnable, string intToDisable)
+        {
+            //DateTime dt;
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C netsh interface set interface \"Ethernet " + intToEnable + "\" admin=enable";
+            process.StartInfo = startInfo;
+            WriteLine("Enabling interface \"Ethernet " + intToEnable + "\".", richTextBox1);
+            process.Start();
+            WriteLine("Done. Waiting 4s...", richTextBox1);
+            Thread.Sleep(4 * 1000);
+            startInfo.Arguments = "/C netsh interface set interface \"Ethernet " + intToDisable + "\" admin=disable";
+            process.StartInfo = startInfo;
+            WriteLine("Disabling interface \"Ethernet " + intToDisable + "\".", richTextBox1);
+            process.Start();
+            WriteLine("Done.", richTextBox1);
+            WriteLine("Swapped Ethernet " + intToDisable + " (disabled) with Ethernet " + intToEnable + " (enabled).", richTextBox2);
+            LogToFile("Swapped Ethernet " + intToDisable + " (disabled) with Ethernet " + intToEnable + " (enabled).");
+        }
+
+        public void LogToFile(string message)
+        {
+            DateTime now = DateTime.Now;
+            string msg = now + " | " + message;
+            try
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(_txtPath));
+            }
+            catch (IOException e)
+            {
+                MessageBox.Show("Error: " + e.Message);
+            }
+
+            //try
+            //{
+            //    if (!Directory.Exists(_path))
+            //    {
+            //        Directory.CreateDirectory(_path);
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+
+            //}
+            
+
+            File.AppendAllLines(_txtPath, new[]
+            {
+                msg
+            });
+            WriteLine("Wrote line to log file: " + _txtPath, richTextBox1);
+
+            //try
+            //{
+            //    if (!File.Exists(txtPath))
+            //    {
+            //        using (StreamWriter sw = File.CreateText(txtPath))
+            //        {
+            //            //create empty file
+            //            sw.Write("");
+            //        }
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    WriteLine(e.Message);
+            //}
+
+
+            //using (StreamWriter sw = File.AppendText(txtPath))
+            //{
+            //    sw.WriteLine(msg);
+            //    WriteLine("Wrote line to log file: " + txtPath);
+            //}
+        }
+
+        public void MinuteToolStrip()
+        {
+            if (_totalSwaps > 0)
+            {
+                DateTime now = DateTime.Now;
+                TimeSpan ts = now - _runtime;
+                TimeSpan ts2 = now - _lastSwapTime;
+                double minutes = ts.TotalMinutes;
+                if (minutes > 1)
+                {
+                    _swapsPerHour = (_totalSwaps / (minutes / 60));
+                }
+                else
+                {
+                    _swapsPerHour = (double)_totalSwaps;
+                }
+                swapsPerHour.Text = "Avg Swaps Per Hour: " + _swapsPerHour.ToString("n2");
+                String laast = "Last Swap: " + _lastSwapTime + " - ";
+                if (ts2.TotalHours < 1 && ts2.TotalMinutes < 1)
+                {
+                    laast += "Just now";
+                }
+                else if (ts2.TotalHours < 1)
+                {
+                    laast += ts2.Minutes + " minutes ago";
+                }
+                else if (ts2.Hours > 0)
+                {
+                    laast += ts2.Hours + " hours " + ts2.Minutes + " minutes ago";
+                }
+                //String laaast = $"Last Swap: {_lastSwapTime} - {((ts2.Hours < 1 && ts2.Minutes < 5) ? "Just now" : "{ts2.Hours} hours {ts2.Minutes} minutes ago")}";
+                lastSwapTS.Text = laast;
+            }
+        }
+
+        public void UpdateToolstrip()
+        {
+            _lastSwapTime = DateTime.Now;
+            _totalSwaps++;
+            totalSwaps.Text = "Total Swaps: " + _totalSwaps;
+            MinuteToolStrip();
+        }
+    }
+}
